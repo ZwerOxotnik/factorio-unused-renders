d06c6d258add940404be0cebecbed3aa9249c38d
ZwerOxotnik <zweroxotnik@gmail.com>
2024-12-06 16:15:42 +0300

    Add items_data_path.lua

---
+++ items_data_path.lua
@@ -0,0 +1,5 @@
+return {
+    yellowcake = "__unused-renders-m__/prototypes/item/yellowcake",
+    ["yellowcake-chunk"] = "__unused-renders-m__/prototypes/item/yellowcake-chunk",
+    ["fluoride-crystal"] = "__unused-renders-m__/prototypes/item/fluoride-crystal",
+}
