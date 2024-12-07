0df2cefe046f54ecc644dfca832ee44bf71170bf
ZwerOxotnik <zweroxotnik@gmail.com>
2024-12-07 21:29:54 +0300

    Add recommended_item_scales.lua

---
+++ recommended_item_scales.lua
@@ -0,0 +1,13 @@
+return {
+    ["__unused-renders-m__/item/mipped/material-crystal-fluorite-1.png"] = 0.5,
+    ["__unused-renders-m__/item/mipped/material-crystal-fluorite-2.png"] = 0.5,
+    ["__unused-renders-m__/item/mipped/material-crystal-fluorite-3.png"] = 0.5,
+    ["__unused-renders-m__/item/mipped/material-crystal-fluorite-4.png"] = 0.5,
+    ["__unused-renders-m__/item/mipped/pile-chunk-yellowcake-4.png"] = 0.4,
+    ["__unused-renders-m__/item/mipped/pile-chunk-yellowcake-5.png"] = 0.4,
+    ["__unused-renders-m__/item/mipped/pile-chunk-yellowcake-6.png"] = 0.4,
+    ["__unused-renders-m__/item/mipped/pile-chunk-yellowcake-7.png"] = 0.4,
+    ["__unused-renders-m__/item/mipped/pile-dust-yellowcake-1.png"] = 0.4,
+    ["__unused-renders-m__/item/mipped/pile-dust-yellowcake-2.png"] = 0.4,
+    ["__unused-renders-m__/item/mipped/pile-dust-yellowcake-3.png"] = 0.4,
+}
